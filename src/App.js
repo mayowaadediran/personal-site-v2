288ccb3f8fe22db04c3844dfc3181522d47a019c
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2019-04-29 10:29:51 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Switch} from 'react-router-dom';
+import Nav from './Component/Nav';
+import About from './Component/About';
+import Home from './Component/Home';
+import Portfolio from './Component/Portfolio';
+import Blog from './Component/Blog';
+import Footer from './Component/Footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Nav/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/about" component={About} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/blog" component={Blog} />
+        </Switch>
+        <Footer />
       </div>
     );
   }
