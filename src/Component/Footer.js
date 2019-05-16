e502416d33264c36006335c804c6d2f3ef5b5e81
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2019-05-16 18:21:31 +0100

    fix social links

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -12,20 +12,20 @@ import '../styles/main.css';
             &copy; 2019 | Crafted by Mayowa Adediran
           </p>
         </div>
-      <div className="footer-social-links">
+        <div className="footer-social-links">
           <a href="https://github.com/mayowaadediran" target="_blank" rel='noreferrer noopener' className="social-link">
             <FontAwesomeIcon icon={["fab", "github"]}/>
           </a>
           <a href="https://twitter.com/ace_mayor" target="_blank" rel='noreferrer noopener' className="social-link">
             <FontAwesomeIcon icon={["fab", "twitter"]}/>
           </a>
-          <a href="https://www.linkedin.com/in/mayowa-adediran/" target="_blank"rel='noreferrer noopener' className="social-link">
+          <a href="https://www.linkedin.com/in/mayowa-adediran/" target="_blank" rel='noreferrer noopener' className="social-link">
             <FontAwesomeIcon icon={["fab", "linkedin-in"]} />
           </a>
           <a href="https://medium.com/@ace_mayor" target="_blank" rel='noreferrer noopener' className="social-link">
             <FontAwesomeIcon icon={["fab", "medium"]}/>
           </a>
-      </div>
+        </div>
       </div>
     )
   }
