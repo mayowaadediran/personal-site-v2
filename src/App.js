70c1d391111d3a2d84ada8516535285df60bd89d
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2019-05-16 18:21:04 +0100

    remove unneccessary import

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas  } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 
-
-
 library.add(fab, fas)
 
 class App extends Component {
