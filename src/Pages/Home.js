27822a9ff8aebc65f9b3d3a666cbb1203421591e
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2019-05-16 18:23:30 +0100

    refactor component markup

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,32 +5,23 @@ class Home extends Component {
   render() {
     return (
       <div className="home container">
-        <div className="hi-content">
-          <div>
-            <h1>
-              Hi, I'm Mayowa
-            </h1>
-            <h2>
-              A <span className="strong-span">Frontend Web Developer</span> from Lagos, Nigeria. I love creating things that work on
-              the internet with a user-centric and simplistic approach. When I'm not coding, I'm not coding.
-            </h2>
-            <p>
-              Get in touch
-              <span>
-                <a href="mailto:ddrn_mayowa@yahoo.com" className="email">
-                  ddrn_mayowa@yahoo.com
-                </a>
-              </span>
-            </p>
-          </div>
+        <div className="home-content">
+          <h1>
+            Hi, I'm Mayowa
+          </h1>
+          <h2>
+            A <span className="strong-span">Frontend Web Developer</span> from Lagos, Nigeria. I love creating things that work on
+            the internet with a user-centric and simplistic approach. When I'm not coding, I'm not coding.
+          </h2>
+          <p>
+            Get in touch
+            <span>
+              <a href="mailto:ddrn_mayowa@yahoo.com" className="email">
+                ddrn_mayowa@yahoo.com
+              </a>
+            </span>
+          </p>
         </div>
-        
-        <div>
-          <div className="hi-image">
-            <img src="" alt="" />
-          </div>
-        </div>
-        
       </div>
     )
   }
